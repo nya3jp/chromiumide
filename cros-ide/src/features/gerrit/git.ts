919823be0abb2575d95c394b25d1286d72b51558
Keigo Oka <oka@google.com>
2023-09-01 05:10:54 +0000

    ide: Fix preupload error
    Remove an unused import.
    
    BUG=None
    TEST=npm run preupload
    
    Change-Id: Ie32a416f15d08331d0a5cdb14fc021beb51da54d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/4835433
    Tested-by: Keigo Oka <oka@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- cros-ide/src/features/gerrit/git.ts
+++ cros-ide/src/features/gerrit/git.ts
@@ -7,7 +7,6 @@ import * as os from 'os';
 import * as path from 'path';
 import * as commonUtil from '../../common/common_util';
 import {envForDepotTools} from '../../common/depot_tools';
-import {underDevelopment} from '../../services/config';
 import {Sink} from './sink';
 
 /** Kind of a Git remote repository */
