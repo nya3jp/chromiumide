239a4a8f396a499fc1522c77840d21502b89e741
Grace Cham <hscham@chromium.org>
2023-11-29 13:34:44 +0900

    ide: provide utility function to query use flag for board/package
    For seamless deployment work which would suggest image with a matching
    cros-debug flag with the package.
    
    Handle error of known common issues to give better feedback to user:
    board or package does not exist.
    
    BUG=b:308059094
    TEST=npm t
    TEST=manual: run getUseFlagsInstalled() on board before setting up and \
         fake packages (foo - one suggestion, abcdef - no suggestion, \
         libchro - multiple suggestions)
    
    Change-Id: I890f94437397bda4c30be06c2d4e51c02d3003b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/infra/ide/+/5215689
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Grace Cham <hscham@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Auto-Submit: Grace Cham <hscham@chromium.org>

--- chromiumide/src/common/chromiumos/portage/equery.ts
+++ chromiumide/src/common/chromiumos/portage/equery.ts
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {chromiumos} from '../../../services';
+import {AbnormalExitError} from '../../common_util';
 import {BoardOrHost} from '../board_or_host';
 import {ParsedPackageName} from './ebuild';
 
@@ -21,4 +23,83 @@ export function buildGet9999EbuildCommand(
     'which',
     `=${pkg.category}/${pkg.name}-9999`,
   ];
+}
+
+/*
+ * Returns all use flags for installation on given board for given package, or error if the command
+ * fails to execute.
+ *
+ * Note that we want use flags use flags used on building the binary package but not the setting for
+ * (new) installation because `cros deploy` only copies the binary packages built and does not build
+ * the package.
+ */
+export async function getUseFlagsInstalled(
+  board: BoardOrHost,
+  targetPackage: string,
+  chrootService: chromiumos.ChrootService
+): Promise<Map<string, boolean> | Error> {
+  // `equery uses` is normally the command to get use flags. It provides two states, 'U - final flag
+  // setting for installation' and (the second one) 'I - package is installed with flag'.
+  // However, with ChrootService.exec, `equery uses` strips "noises" and gives a simple output that
+  // contains only the 'U' flag but not the 'I' flag.
+  // So we have to parse the more complicated output from `emerge` instead.
+  const args = [
+    board.suffixedExecutable('emerge'),
+    '--pretend', // to not really rebuild the package
+    '--verbose', // to show USE flags
+    '--nodeps', // to avoid unnecessary dependency calculation
+    '--usepkgonly', // to print the USE flags from the binary package
+    targetPackage,
+  ];
+  const result = await chrootService.exec(args[0], args.slice(1), {
+    sudoReason: `to get ${targetPackage} use flags on ${board.toBoardName()}`,
+  });
+  if (result instanceof Error) {
+    if (result instanceof AbnormalExitError && result.exitStatus === 127) {
+      return Error(
+        `Failed to get USE flags of ${targetPackage} on ${board}: command not found: have you setup board ${board} on chroot?`
+      );
+    }
+    if (
+      result instanceof AbnormalExitError &&
+      result.exitStatus === 1 &&
+      result.stderr.includes('emerge: there are no binary packages to satisfy ')
+    ) {
+      const suggestedPackageMsgRe = /emerge: Maybe you meant .*/;
+      const suggestedPackageMsgMatch = result.stderr.match(
+        suggestedPackageMsgRe
+      );
+      return Error(
+        `Failed to get USE flags of ${targetPackage} on ${board}: binary package not found: ${
+          suggestedPackageMsgMatch
+            ? suggestedPackageMsgMatch[0]
+            : 'emerge: nothing similar found.'
+        }`
+      );
+    }
+    return result;
+  }
+
+  const flags = new Map<string, boolean>();
+
+  // First get the list of all USE flags (a string of space-delimited flag settings).
+  const useFlagsRe = /USE="([^"]*)"/;
+  const match = useFlagsRe.exec(result.stdout);
+  // Assume package does not have any USE flag, return empty map.
+  if (!match) return flags;
+
+  // Legal characters for use flag names: https://projects.gentoo.org/pms/8/pms.html#x1-200003.1.4
+  // A '-' prefix means the flag is unset:
+  //   https://dev.gentoo.org/~zmedico/portage/doc/man/emerge.1.html.
+  const flagRe = /(-)?([A-Za-z0-9+_@-]+)/;
+  for (const flag of match[1].split(' ')) {
+    const fmatch = flagRe.exec(flag);
+    if (!fmatch) {
+      return Error(
+        `Failed to parse USE flag "${flag}" for ${targetPackage} on ${board}: must match ${flagRe.source}`
+      );
+    }
+    flags.set(fmatch[2], fmatch[1] !== '-');
+  }
+  return flags;
 }
---
+++ chromiumide/src/test/unit/common/chromiumos/portage/equery.test.ts
@@ -0,0 +1,255 @@
+// Copyright 2024 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {BoardOrHost} from '../../../../../common/chromiumos/board_or_host';
+import {getUseFlagsInstalled} from '../../../../../common/chromiumos/portage/equery';
+import * as commonUtil from '../../../../../common/common_util';
+import * as services from '../../../../../services';
+import * as testing from '../../../../testing';
+import * as fakes from '../../../../testing/fakes';
+
+function installEmergeForUseFlagsCommandHandler(
+  fakeExec: testing.FakeExec,
+  sourcePath: string,
+  board: BoardOrHost,
+  packageName: string,
+  stdout: string,
+  stderr?: string,
+  exitSatus?: number
+): void {
+  const cmd = `emerge${board.isHost ? '' : `-${board.toBoardName()}`}`;
+  const args = [
+    '--pretend',
+    '--verbose',
+    '--nodeps',
+    '--usepkgonly',
+    packageName,
+  ];
+  fakes.installChrootCommandHandler(
+    fakeExec,
+    sourcePath as commonUtil.Source,
+    cmd,
+    args,
+    async () =>
+      exitSatus
+        ? new commonUtil.AbnormalExitError(
+            cmd,
+            args,
+            exitSatus,
+            stdout,
+            stderr ?? ''
+          )
+        : stdout
+  );
+}
+
+describe('equery use flag', () => {
+  const {fakeExec} = testing.installFakeExec();
+  fakes.installFakeSudo(fakeExec);
+  const tempDir = testing.tempDir();
+
+  it('on board trogdor', async () => {
+    await testing.buildFakeChroot(tempDir.path);
+
+    const board = BoardOrHost.newBoard('trogdor');
+    const packageName = 'chromeos-base/libchrome';
+    const fakeStdout = `
+These are the packages that would be merged, in order:
+
+[binary   R   *] chromeos-base/libchrome-9999:0/9999::chromiumos to /build/trogdor/ USE="cros-debug msan -dbus -asan -cfi -cfi_diag" 0 KiB
+
+Total: 1 package (1 reinstall, 1 binary), Size of downloads: 0 KiB
+`;
+
+    installEmergeForUseFlagsCommandHandler(
+      fakeExec,
+      tempDir.path,
+      board,
+      packageName,
+      fakeStdout
+    );
+
+    const flags = await getUseFlagsInstalled(
+      board,
+      packageName,
+      services.chromiumos.ChrootService.maybeCreate(tempDir.path, false)!
+    );
+    expect(flags).toBeInstanceOf(Map<string, boolean>);
+    expect(flags).toEqual(
+      new Map<string, boolean>([
+        ['cros-debug', true],
+        ['msan', true],
+        ['dbus', false],
+        ['asan', false],
+        ['cfi', false],
+        ['cfi_diag', false],
+      ])
+    );
+  });
+
+  it('on host', async () => {
+    await testing.buildFakeChroot(tempDir.path);
+
+    const board = BoardOrHost.HOST;
+    const packageName = 'chromeos-base/libbrillo';
+    const fakeStdout = `
+These are the packages that would be merged, in order:
+
+[binary   R    ] chromeos-base/libbrillo-0.0.1-r2397:0/0.0.1-r2397::chromiumos  USE="cros_host dbus udev -asan -compilation_database" 0 KiB
+
+Total: 1 package (1 reinstall, 1 binary), Size of downloads: 0 KiB
+`;
+    installEmergeForUseFlagsCommandHandler(
+      fakeExec,
+      tempDir.path,
+      board,
+      packageName,
+      fakeStdout
+    );
+
+    const flags = await getUseFlagsInstalled(
+      board,
+      packageName,
+      services.chromiumos.ChrootService.maybeCreate(tempDir.path, false)!
+    );
+    expect(flags).toBeInstanceOf(Map<string, boolean>);
+    expect(flags).toEqual(
+      new Map<string, boolean>([
+        ['cros_host', true],
+        ['dbus', true],
+        ['udev', true],
+        ['asan', false],
+        ['compilation_database', false],
+      ])
+    );
+  });
+
+  it('fails with command not found error', async () => {
+    await testing.buildFakeChroot(tempDir.path);
+
+    const board = BoardOrHost.newBoard('hatch');
+    const packageName = 'chromeos-base/libchrome';
+    installEmergeForUseFlagsCommandHandler(
+      fakeExec,
+      tempDir.path,
+      board,
+      packageName,
+      '',
+      'env: ‘emerge-hatch’: No such file or directory',
+      127
+    );
+
+    expect(
+      (
+        (await getUseFlagsInstalled(
+          board,
+          packageName,
+          services.chromiumos.ChrootService.maybeCreate(tempDir.path, false)!
+        )) as Error
+      ).message
+    ).toContain('command not found: have you setup board hatch on chroot?');
+  });
+
+  it('fails with no binary package error with no suggestion', async () => {
+    await testing.buildFakeChroot(tempDir.path);
+
+    const board = BoardOrHost.newBoard('trogdor');
+    const packageName = 'abcdef';
+    const fakeStdErr = `
+emerge: there are no binary packages to satisfy "abcdef" for /build/trogdor/.
+
+emerge: searching for similar names... nothing similar found.
+`;
+    installEmergeForUseFlagsCommandHandler(
+      fakeExec,
+      tempDir.path,
+      board,
+      packageName,
+      '',
+      fakeStdErr,
+      1
+    );
+
+    expect(
+      (
+        (await getUseFlagsInstalled(
+          board,
+          packageName,
+          services.chromiumos.ChrootService.maybeCreate(tempDir.path, false)!
+        )) as Error
+      ).message
+    ).toContain('binary package not found: emerge: nothing similar found.');
+  });
+
+  it('fails with no binary package error with single suggestion', async () => {
+    // Do not install fake chroot command handler i.e. should give command not found error.
+    await testing.buildFakeChroot(tempDir.path);
+
+    const board = BoardOrHost.newBoard('trogdor');
+    const packageName = 'foo';
+    const fakeStdErr = `
+emerge: there are no binary packages to satisfy "foo" for /build/trogdor/.
+
+emerge: searching for similar names...
+emerge: Maybe you meant sys-block/fio?
+`;
+
+    installEmergeForUseFlagsCommandHandler(
+      fakeExec,
+      tempDir.path,
+      board,
+      packageName,
+      '',
+      fakeStdErr,
+      1
+    );
+
+    expect(
+      (
+        (await getUseFlagsInstalled(
+          BoardOrHost.newBoard('trogdor'),
+          'foo',
+          services.chromiumos.ChrootService.maybeCreate(tempDir.path, false)!
+        )) as Error
+      ).message
+    ).toContain(
+      'binary package not found: emerge: Maybe you meant sys-block/fio?'
+    );
+  });
+
+  it('fails with no binary package error with multiple suggestions', async () => {
+    // Do not install fake chroot command handler i.e. should give command not found error.
+    await testing.buildFakeChroot(tempDir.path);
+
+    const board = BoardOrHost.newBoard('trogdor');
+    const packageName = 'libchro';
+    const fakeStdErr = `
+emerge: there are no binary packages to satisfy "libchro" for /build/trogdor/.
+
+emerge: searching for similar names...
+emerge: Maybe you meant any of these: chromeos-base/libchrome, dev-libs/libcroco, dev-rust/libchromeos?
+`;
+    installEmergeForUseFlagsCommandHandler(
+      fakeExec,
+      tempDir.path,
+      board,
+      packageName,
+      '',
+      fakeStdErr,
+      1
+    );
+
+    expect(
+      (
+        (await getUseFlagsInstalled(
+          board,
+          packageName,
+          services.chromiumos.ChrootService.maybeCreate(tempDir.path, false)!
+        )) as Error
+      ).message
+    ).toContain(
+      'binary package not found: emerge: Maybe you meant any of these: chromeos-base/libchrome, dev-libs/libcroco, dev-rust/libchromeos?'
+    );
+  });
+});
