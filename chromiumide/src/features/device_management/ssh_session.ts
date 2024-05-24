99a011545e2a6f52d2bdd76fd2fcb3368ac30893
Keigo Oka <oka@google.com>
2024-05-24 05:15:19 +0000

    ssh: refactor not to throw error
    Follow up of https://chromium-review.googlesource.com/c/chromiumos/infra/ide/+/5561927/comment/3a3424da_dc596a68/
    
    BUG=b:265155874
    TEST=npm t
    
    Change-Id: Ibc4097413c3eb390a9b4b1df4469d6ca8ff9a618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/infra/ide/+/5568449
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Grace Cham <hscham@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>

--- chromiumide/src/features/device_management/ssh_session.ts
+++ chromiumide/src/features/device_management/ssh_session.ts
@@ -6,6 +6,7 @@ import * as net from 'net';
 import * as vscode from 'vscode';
 import * as commonUtil from '../../../shared/app/common/common_util';
 import {CancelledError} from '../../../shared/app/common/exec/types';
+import {assertNever} from '../../../shared/app/common/typecheck';
 import {MemoryOutputChannel} from '../../common/memory_output_channel';
 import {TeeOutputChannel} from '../../common/tee_output_channel';
 import {
@@ -93,30 +94,27 @@ async function startSshConnection(
   const checkTunnelIsUp = waitSshServer(forwardPort, token);
 
   // Wait until connection with server starts, or fails to start.
-  try {
-    await Promise.race([startTunnelAndWait, checkTunnelIsUp]);
-  } catch (e) {
-    const err = e as Error;
-
-    if (err instanceof DiagnosedError) {
-      showErrorMessageWithButtons(
-        `SSH server failed: ${err.message}`,
-        err.buttons
-      );
-    } else {
-      void vscode.window.showErrorMessage(`SSH server failed: ${err}`);
-    }
-
-    output.appendLine(`SSH server failed: ${err}\n${err.stack}`);
-    return false;
+  const err = await Promise.race([startTunnelAndWait, checkTunnelIsUp]);
+  if (!err) {
+    return true;
   }
-  return true;
+  if (!(err instanceof Error)) assertNever(err);
+
+  if (err instanceof DiagnosedError) {
+    showErrorMessageWithButtons(
+      `SSH server failed: ${err.message}`,
+      err.buttons
+    );
+  } else {
+    void vscode.window.showErrorMessage(`SSH server failed: ${err}`);
+  }
+
+  output.appendLine(`SSH server failed: ${err}\n${err.stack}`);
+  return false;
 }
 
 /**
  * This call will block indefinitely until tunnel is exited or an error occurs
- *
- * @throws Error, or DiagnosedError in case of SSH error.
  */
 async function createTunnelAndWait(
   hostname: string,
@@ -124,7 +122,7 @@ async function createTunnelAndWait(
   output: vscode.OutputChannel,
   token: vscode.CancellationToken,
   sshIdentity: SshIdentity
-) {
+): Promise<undefined | Error | DiagnosedError> {
   const SSH_PORT = 22;
   const args = sshUtil.buildSshCommand(hostname, sshIdentity, [
     '-L',
@@ -141,9 +139,9 @@ async function createTunnelAndWait(
     return;
   }
   if (result instanceof Error) {
-    throw diagnoseSshError(result, memoryOutput.output);
+    return diagnoseSshError(result, memoryOutput.output);
   }
-  throw new Error('SSH server stopped unexpectedly');
+  return new Error('SSH server stopped unexpectedly');
 }
 
 async function waitSshServer(
@@ -156,29 +154,31 @@ async function waitSshServer(
     const throttle = new Promise<void>(resolve => {
       setTimeout(resolve, INTERVAL);
     });
-    try {
-      return await checkSshConnection(sshPort);
-    } catch (err: unknown) {
-      // Continue
+    if (await checkSshConnection(sshPort)) {
+      return;
     }
     await throttle;
   }
 }
 
-// Connects to the specified port on localhost to see if a SSH connection is open.
-async function checkSshConnection(sshPort: number): Promise<void> {
-  return new Promise((resolve, reject) => {
+/**
+ * Connects to the specified port on localhost to see if a SSH connection is open.
+ *
+ * @returns true on success, false otherwise.
+ */
+async function checkSshConnection(sshPort: number): Promise<boolean> {
+  return new Promise(resolve => {
     const socket = net.createConnection(sshPort, 'localhost');
     socket.on('data', () => {
       socket.destroy();
-      resolve();
+      resolve(true);
     });
     socket.on('error', () => {
       // Ignore errors.
     });
     socket.on('close', () => {
       socket.destroy();
-      reject();
+      resolve(false);
     });
   });
 }
