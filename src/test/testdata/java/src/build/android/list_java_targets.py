0e3471ac157d29e9fe64b94f73d6011f29bb0403
Shuhei Takahashi <nya@chromium.org>
2025-12-25 11:03:51 +0900

    Add missing license headers

--- src/test/testdata/java/src/build/android/list_java_targets.py
+++ src/test/testdata/java/src/build/android/list_java_targets.py
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Copyright 2025 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 echo "//chrome:chrome: ${PWD}/out/current_link/gen/chrome/chrome_java.params.json"
 echo "//content:content: ${PWD}/out/current_link/gen/content/content_java.params.json"
--- src/test/testdata/java/src/chrome/java/org/chromium/chrome/Browser.java
+++ src/test/testdata/java/src/chrome/java/org/chromium/chrome/Browser.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 package org.chromium.chrome;
 
 import org.chromium.content.Tab;
